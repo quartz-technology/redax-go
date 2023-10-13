4973e48a101785b3fd4e9ff296262f5965202a8b
Luca Georges Francois <luca@quartz.technology>
2023-10-13 21:53:20 +0200

    feat: add ROs definitions
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

---
+++ ro/proposer_payload_delivered.go
@@ -0,0 +1,14 @@
+package ro
+
+import (
+	"math/big"
+
+	v1 "github.com/attestantio/go-builder-client/api/v1"
+)
+
+type ProposerPayloadDeliveredRO struct {
+	v1.BidTrace
+
+	BlockNumber *big.Int `json:"block_number"`
+	NumTx       uint     `json:"num_tx"`
+}
