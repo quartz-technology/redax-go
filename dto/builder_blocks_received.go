980acc5d5335a7c5beaa81f2e2307ef8b048144a
Luca Georges Francois <luca@quartz.technology>
2023-10-13 21:28:13 +0200

    feat: add DTOs definitions
    Signed-off-by: Luca Georges Francois <luca@quartz.technology>

---
+++ dto/builder_blocks_received.go
@@ -0,0 +1,49 @@
+package dto
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type BuilderBlocksReceived struct {
+	Slot             phase0.Slot
+	BlockHash        *common.Hash
+	BlockNumber      *big.Int
+	BuilderPublicKey *phase0.BLSPubKey
+	Limit            uint
+}
+
+func (o *BuilderBlocksReceived) EncodeToQueryParams() string {
+	var args []string
+
+	if o.Slot > 0 {
+		args = append(args, fmt.Sprintf("slot=%d", o.Slot))
+	}
+
+	if o.BlockHash != nil {
+		args = append(args, fmt.Sprintf("block_hash=%s", o.BlockHash))
+	}
+
+	if o.BlockNumber != nil {
+		args = append(args, fmt.Sprintf("block_number=%d", o.BlockNumber))
+	}
+
+	if o.BuilderPublicKey != nil {
+		args = append(args, fmt.Sprintf("block_pubkey=%s", o.BuilderPublicKey))
+	}
+
+	if o.Limit > 0 {
+		args = append(args, fmt.Sprintf("limit=%d", o.Limit))
+	}
+
+	params := strings.Join(args, "&")
+	if len(params) > 0 {
+		params = "?" + params
+	}
+
+	return params
+}
---
+++ dto/proposer_payload_delivered.go
@@ -0,0 +1,69 @@
+package dto
+
+import (
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type ResultsOrder string
+
+const (
+	IncreasingValue ResultsOrder = "value"
+	DecreasingValue ResultsOrder = "-value"
+)
+
+type ProposerPayloadDelivered struct {
+	Slot              phase0.Slot
+	Cursor            uint
+	BlockHash         *common.Hash
+	BlockNumber       *big.Int
+	BuilderPublicKey  *phase0.BLSPubKey
+	ProposerPublicKey *phase0.BLSPubKey
+	Limit             uint
+	OrderBy           ResultsOrder
+}
+
+func (o *ProposerPayloadDelivered) EncodeToQueryParams() string {
+	var args []string
+
+	if o.Slot > 0 {
+		args = append(args, fmt.Sprintf("slot=%d", o.Slot))
+	}
+
+	if o.Cursor > 0 {
+		args = append(args, fmt.Sprintf("cursor=%d", o.Cursor))
+	}
+
+	if o.BlockHash != nil {
+		args = append(args, fmt.Sprintf("block_hash=%s", o.BlockHash))
+	}
+
+	if o.BlockNumber != nil {
+		args = append(args, fmt.Sprintf("block_number=%d", o.BlockNumber))
+	}
+
+	if o.BuilderPublicKey != nil {
+		args = append(args, fmt.Sprintf("builder_pubkey=%s", o.BuilderPublicKey))
+	}
+
+	if o.ProposerPublicKey != nil {
+		args = append(args, fmt.Sprintf("proposer_pubkey=%s", o.ProposerPublicKey))
+	}
+
+	if o.Limit > 0 {
+		args = append(args, fmt.Sprintf("limit=%d", o.Limit))
+	}
+
+	args = append(args, fmt.Sprintf("order_by=%s", o.OrderBy))
+
+	params := strings.Join(args, "&")
+	if len(params) > 0 {
+		params = "?" + params
+	}
+
+	return params
+}
